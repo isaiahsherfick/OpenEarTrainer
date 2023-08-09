a1762363b1bde16cd1dcda4ab418f06867aa1c19
Yuheng Chen <ccyyhh97@hotmail.com>
2023-08-08 22:34:01 -0500

    settings obj

---
+++ Settings.ts
@@ -0,0 +1,39 @@
+const SettingsData = {
+    trainingMode: 'Passive',
+    notesMode: 'Chords',
+    chords: {
+        range: {
+            low: 'A1',
+            high: 'C8'
+        },
+        chordsToQuiz: {
+            major: true,
+            minor: true,
+            diminished: true,
+            augmented: true
+        }
+    },
+    intervals: {
+        range: {
+            low: 'A1',
+            high: 'C8'
+        },
+        intervalsToQuiz: {
+            unison: true,
+            minorSecond: true,
+            majorSecond: true,
+            minorThird: true,
+            majorThird: true,
+            perfectFourth: true,
+            perfectFifth: true,
+            tritone: true,
+            minorSixth: true,
+            majorSixth: true,
+            minorSeventh: true,
+            majorSeventh: true,
+            octave: true
+        }
+    }
+}
+
+export default SettingsData
