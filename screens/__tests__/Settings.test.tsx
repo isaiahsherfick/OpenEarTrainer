7c9f59a1acaf7fda8c1aedffde75d61ccf0640b3
TheAlmightyHelix <ccyyhh97@hotmail.com>
2023-08-28 18:55:36 -0500

    frontend test setup

---
+++ screens/__tests__/Settings.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import SettingsScreen from '../SettingsScreen';
+
+describe('Settings Screen', () => {
+    // const NavigatorMock = renderer.create()
+    // const SettingsScreenMock = renderer.create(<SettingsScreen route={ } navigation={ } />)
+
+    it('renders correctly', () => {
+        // snapshot test
+    })
+})
+
+describe('Back Button', () => {
+    it('takes the user to the previous training screen')
+
+    it('updates the global settings context (state)')
+})
