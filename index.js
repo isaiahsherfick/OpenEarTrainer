fa1178738919d9ffb2cb4103ba2cb32013ba8134
TheAlmightyHelix <ccyyhh97@hotmail.com>
2023-08-28 13:36:07 -0500

    imported local fonts

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
  * @format
  */
 
-import {AppRegistry, Platform} from 'react-native';
+import { AppRegistry, Platform } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
-if (Platform.OS === 'ios') {
-    AppRegistry.registerComponent('main', () => App); 
-} else {
-    AppRegistry.registerComponent(appName, () => App);
-}
+// if (Platform.OS === 'ios') {
+//     AppRegistry.registerComponent('main', () => App); 
+// } else {
+AppRegistry.registerComponent(appName, () => App);
+// }
