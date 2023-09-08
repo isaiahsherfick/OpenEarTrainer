ba1fa608087e4af7ed011cc11b29661db320eabf
Isaiah Sherfick <isaiahsherfick8@gmail.com>
2023-09-08 18:49:44 -0500

    fix settings test failing

--- screens/__tests__/Settings.test.tsx
+++ screens/__tests__/Settings.test.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import SettingsScreen from '../SettingsScreen';
+import React from "react";
+import renderer from "react-test-renderer";
+import SettingsScreen from "../SettingsScreen";
 
-describe('Settings Screen', () => {
-    // const NavigatorMock = renderer.create()
-    // const SettingsScreenMock = renderer.create(<SettingsScreen route={ } navigation={ } />)
+describe("Settings Screen", () => {
+  // const NavigatorMock = renderer.create()
+  // const SettingsScreenMock = renderer.create(<SettingsScreen route={ } navigation={ } />)
 
-    it('renders correctly', () => {
-        // snapshot test
-    })
-})
+  it("renders correctly", () => {
+    // snapshot test
+  });
+});
 
-describe('Back Button', () => {
-    it('takes the user to the previous training screen')
+describe("Back Button", () => {
+  it("takes the user to the previous training screen", () => {
+  });
 
-    it('updates the global settings context (state)')
-})
+  it("updates the global settings context (state)", () => {
+  });
+});
